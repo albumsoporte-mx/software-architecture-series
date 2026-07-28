17ab4ecf1fd3fbbdbf6251550fa3f9695218693f
luis enrique gonzalez arellano <luis.gonzalez@puebla.gob.mx>
2026-07-28 12:48:05 -0600

    feat:opentelemetry in ms

--- ms-ventas/app/main.py
+++ ms-ventas/app/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+# 1. Configuramos el proveedor principal de trazas (TracerProvider)
+provider = TracerProvider()
+trace.set_tracer_provider(provider)
+
+# 2. Apuntamos el exportador OTLP al puerto 4318 de nuestro OTel Collector en la red interna
+otlp_exporter = OTLPSpanExporter(endpoint="http://inspector_otel:4318/v1/traces")
+processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(processor)
+
+
 
 # El root_path hace la magia: le avisa a Swagger UI que estamos detrás de un Gateway
 app = FastAPI(
     title="Microservicio de Ventas",
     root_path="/api/ventas"
 )
+
+# 3. ¡Magia pura! Auto-instrumentamos FastAPI para que capture cada petición automáticamente
+FastAPIInstrumentor.instrument_app(app)
+
 
 # Nota que aquí dejamos la ruta limpia, sin el prefijo
 @app.get("/get-ventas")
